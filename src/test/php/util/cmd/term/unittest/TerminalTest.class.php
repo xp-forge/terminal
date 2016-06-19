ac8c280264bdcd7669b481e1e931a18e006e6f9a
Timm Friebe <thekid@thekid.de>
2016-06-19 23:17:17 +0200

    Use ESC[39m and ESC[49m to reset to default colors instead of [0m

--- src/test/php/util/cmd/term/unittest/TerminalTest.class.php
+++ src/test/php/util/cmd/term/unittest/TerminalTest.class.php
@@ -31,21 +31,21 @@ class TerminalTest extends \unittest\TestCase {
 
   #[@test]
   public function format_with_color() {
-    $this->assertFormatted("\e[31;1mTest\e[22;0m", '<red>Test</>');
+    $this->assertFormatted("\e[31;1mTest\e[22;39m", '<red>Test</>');
   }
 
   #[@test]
   public function end_tags_may_be_spelled_out() {
-    $this->assertFormatted("\e[31;1mTest\e[22;0m", '<red>Test</red>');
+    $this->assertFormatted("\e[31;1mTest\e[22;39m", '<red>Test</red>');
   }
 
   #[@test]
   public function format_with_color_and_underline() {
-    $this->assertFormatted("\e[31m\e[4mTest\e[24m\e[0m", '<dark-red,underline>Test</>');
+    $this->assertFormatted("\e[31m\e[4mTest\e[24m\e[39m", '<dark-red,underline>Test</>');
   }
 
   #[@test]
   public function format_with_color_and_nested_attribute() {
-    $this->assertFormatted("\e[31mTest \e[4mLink\e[24m\e[0m", '<dark-red>Test <underline>Link</></>');
+    $this->assertFormatted("\e[31mTest \e[4mLink\e[24m\e[39m", '<dark-red>Test <underline>Link</></>');
   }
 }
