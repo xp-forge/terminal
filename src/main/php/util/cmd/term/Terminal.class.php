f13a835c8570367f84642d352b00f95e502b72f8
Timm Friebe <thekid@thekid.de>
2016-06-19 14:08:01 +0200

    Simplify color set / reset implementation

--- src/main/php/util/cmd/term/Terminal.class.php
+++ src/main/php/util/cmd/term/Terminal.class.php
@@ -13,54 +13,40 @@ class Terminal {
     'underline' => ["\e[4m", "\e[24m"]
   ];
 
-  public static $fg= [
-    'black'        => ["30", "0"],
-    'dark-red'     => ["31", "0"],
-    'dark-green'   => ["32", "0"],
-    'dark-yellow'  => ["33", "0"],
-    'dark-blue'    => ["34", "0"],
-    'dark-magenta' => ["35", "0"],
-    'dark-cyan'    => ["36", "0"],
-    'gray'         => ["37", "0"],
+  public static $colors= [
+    'black'        => ['0', '0'],
+    'dark-red'     => ['1', '0'],
+    'dark-green'   => ['2', '0'],
+    'dark-yellow'  => ['3', '0'],
+    'dark-blue'    => ['4', '0'],
+    'dark-magenta' => ['5', '0'],
+    'dark-cyan'    => ['6', '0'],
+    'gray'         => ['7', '0'],
 
-    'dark-gray'    => ["30;1", "22;0"],
-    'red'          => ["31;1", "22;0"],
-    'green'        => ["32;1", "22;0"],
-    'yellow'       => ["33;1", "22;0"],
-    'blue'         => ["34;1", "22;0"],
-    'magenta'      => ["35;1", "22;0"],
-    'cyan'         => ["36;1", "22;0"],
-    'white'        => ["37;1", "22;0"]
+    'dark-gray'    => ['0;1', '22;0'],
+    'red'          => ['1;1', '22;0'],
+    'green'        => ['2;1', '22;0'],
+    'yellow'       => ['3;1', '22;0'],
+    'blue'         => ['4;1', '22;0'],
+    'magenta'      => ['5;1', '22;0'],
+    'cyan'         => ['6;1', '22;0'],
+    'white'        => ['7;1', '22;0']
   ];
 
-  public static $bg= [
-    'black'        => ["40", "0"],
-    'dark-red'     => ["41", "0"],
-    'dark-green'   => ["42", "0"],
-    'dark-yellow'  => ["43", "0"],
-    'dark-blue'    => ["44", "0"],
-    'dark-magenta' => ["45", "0"],
-    'dark-cyan'    => ["46", "0"],
-    'gray'         => ["47", "0"],
-
-    'dark-gray'    => ["1;40", "22;0"],
-    'red'          => ["1;41", "22;0"],
-    'green'        => ["1;42", "22;0"],
-    'yellow'       => ["1;43", "22;0"],
-    'blue'         => ["1;44", "22;0"],
-    'magenta'      => ["1;45", "22;0"],
-    'cyan'         => ["1;46", "22;0"],
-    'white'        => ["1;47", "22;0"]
-  ];
-
-  public static function format($arg) {
-    preg_match_all('/<(\/|[a-z][a-z@,-]+)>/', $arg, $matches, PREG_OFFSET_CAPTURE);
+  /**
+   * Format a string containing `<styles>`...`</>` sequences.
+   *
+   * @param  string $in
+   * @return string
+   */
+  public static function format($in) {
+    preg_match_all('/<(\/|[a-z][a-z@,-]+)>/', $in, $matches, PREG_OFFSET_CAPTURE);
 
     $formatted= '';
     $offset= 0;
     foreach ($matches[1] as $match) {
       if ($match[1] > 1) {
-        $formatted.= substr($arg, $offset, $match[1] - $offset - 1);
+        $formatted.= substr($in, $offset, $match[1] - $offset - 1);
       }
 
       if ('/' === $match[0]) {
@@ -73,8 +59,8 @@ class Terminal {
             $unset= self::$theme[$style][1].$unset;
           } else {
             sscanf($style, '%[^@]@%s', $fg, $bg);
-            $set.= "\e[".self::$fg[$fg][0].($bg ? ';'.self::$bg[$bg][0] : '').'m';
-            $unset= "\e[".self::$fg[$fg][1].($bg ? ';'.self::$bg[$bg][1] : '').'m'.$unset;
+            $set.= "\e[3".self::$colors[$fg][0].($bg ? ';4'.self::$colors[$bg][0] : '').'m';
+            $unset= "\e[".self::$colors[$fg][1].($bg ? ';'.self::$colors[$bg][1] : '').'m'.$unset;
           }
         }
         $formatted.= $set;
@@ -83,7 +69,6 @@ class Terminal {
 
       $offset= $match[1] + strlen($match[0]) + 1;
     }
-    return $formatted.substr($arg, $offset);
-    // return str_replace("\e", "\\e", $formatted.substr($arg, $offset));
+    return $formatted.substr($in, $offset);
   }
 }
